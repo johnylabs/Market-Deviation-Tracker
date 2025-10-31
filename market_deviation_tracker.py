fbb952c86b0c7b50af8c7f673af5148264a90051
jetbaby <inanenova@protonmail.com>
2025-10-31 03:51:23 -0600

    # 1st run attempt

---
+++ market_deviation_tracker.py
@@ -0,0 +1,15 @@
+# USER IS ASKED WHAT FOOD THEY WANT TO MONITOR
+print(“Please select your commodity”)
+print(“a. COFFEE”)
+print(“b. ORANGE_JUICE”)
+print(“c. CORN”)
+choice = INPUT()
+
+IF choice == a:
+food = “COFFEE”
+ELIF choice == b:
+food = “ORANGE_JUICE”
+ELIF choice == c:
+food = “CORN”
+ELSE:
+print(“A VALID OPTION WAS NOT SELECTED, DEFAULTING TO COFFEE”)
